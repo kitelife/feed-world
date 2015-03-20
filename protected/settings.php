329020bb7c6375eeabc01b9ea487ead196e2f80c
youngsterxyf <sas.198708@gmail.com>
2015-03-20 16:25:07 +0800

    update

--- protected/settings.php
+++ protected/settings.php
@@ -12,22 +12,22 @@ return array(
         'host' => '127.0.0.1',
         'port' => '3306',
         'username' => 'root',
-        'password' => '06122553',
+        'password' => 'root',
         'options' => array(
             \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         ),
     ),
     'github' => array(
-        'client_id' => 'ec7bea72d99faab0d330',
-        'client_secret' => 'e33e9e270f6fe9bae3611f2edbaafea0917c0e5a',
-        'redirect_uri' => 'http://58.215.187.122:9009/user/login',
+        'client_id' => '',
+        'client_secret' => '',
+        'redirect_uri' => '',
         'scope' => '',
         'state' => 'github-login-feed-world'
     ),
     'weibo' => array(
-        'AppKey' => '1837829253',
-        'client_secret' => '28e8750f6b9fa159dc5fce770decac02',
-        'redirect_uri' => 'http://58.215.187.122:9009/user/login',
+        'AppKey' => '',
+        'client_secret' => '',
+        'redirect_uri' => '',
         'scope' => '',
         'state' => 'weibo-login-feed-world'
     ),
